91c7c162bfadb08fafd01e7e813c0321910aa4e9
Dezodev <dezodev@gmail.com>
2018-03-23 16:58:35 +0100

    Delete files

--- 404.php
+++ 404.php
@@ -1,24 +1,15 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section>
+		<article id="post-404">
 
-			<!-- article -->
-			<article id="post-404">
+			<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+			<h2>
+				<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+			</h2>
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
+		</article>
 	</main>
 
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
